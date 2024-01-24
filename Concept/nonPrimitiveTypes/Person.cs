413e023a134b5cf5f39a46537252299b5f5f3ebe
Arif Iqbal <arif_iqbal@yahoo.com>
2024-01-23 23:34:28 -0600

    Initial Commit

---
+++ Concept/nonPrimitiveTypes/Person.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace CSharpFundamentals
+{
+    public class Person
+{
+    public string FirstName;
+    public string LastName;
+
+    public void Introduce()
+    {
+        Console.WriteLine($"My name is {FirstName} {LastName}!");
+    }
+}
+}
---
+++ Concept/nonPrimitiveTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using nonPrimitiveTypes.Math;
+
+namespace CSharpFundamentals
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("NON-Primitive Types!\n");
+
+
+            Person john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
