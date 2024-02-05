87f572615c7798721170cceec3b3414d4a8ced3c
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-05 13:17:47 -0600

    modified:   Concept/strings/sringSummarisation/summarisationText/Program.cs

--- Concept/strings/sringSummarisation/summarisationText/Program.cs
+++ Concept/strings/sringSummarisation/summarisationText/Program.cs
@@ -1,9 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Text Summarisation!");
 
-var sentences = "This is going to be a very extra ordinary perfecdt example of a very very very long paragrah."
-const int maxLength = 20;
+var sentences = "This is going to be a very extra ordinary perfecdt example of a very very very long paragrah.";
+const int maxLength = 24;
 
 if (sentences.Length < maxLength){
     Console.WriteLine(sentences);
+}else{
+    var words = sentences.Split(" ");
+    var totalCharacters = 0;
+    var summaryWords = new List<string>();
+
+    foreach (var word in words){
+        summaryWords.Add(word);
+        // Console.WriteLine(word.Length);
+        totalCharacters += word.Length;
+        if (totalCharacters > maxLength){
+            break;
+        }
+    }
+    var summary = string.Join(" ", summaryWords) + "...";
+    Console.WriteLine(summary);
 }
