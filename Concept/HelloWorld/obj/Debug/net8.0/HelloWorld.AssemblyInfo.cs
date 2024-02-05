b9cfb76ff349d8937272a64e0f7102fc1da1790d
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-05 14:47:55 -0600

    StringBulider Initial Commit

--- Concept/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ Concept/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22bce109ae3a4d4b9e3945484e295bbc6c5032ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56f59f93e8cfcd8f7050d7e1198c383a2bfc1b82")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Concept/stingBuilder/StringBulider/StringBulider/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("String Builder!\n");
+
+        var builder = new StringBuilder();
+
+        builder.Append('_', 10);
+        builder.AppendLine(); // Adds a new empty line
+        builder.Append("Header");
+        builder.AppendLine(); // Adds a new empty line
+        builder.Append('_', 10);
+
+        builder.Replace('_', '*');
+        builder.Remove(0, 10); // Removes the first entity of the builder string
+
+        builder.Insert(0,new string('+', 5));  // Insert goes to the begining of the first builder string entity.
+
+
+        Console.WriteLine(builder);
+
+
+        var builderTwo = new System.Text.StringBuilder("This is Another Example of StringBuilder Class");
+
+        builderTwo
+            .Append('*', 7)
+            .AppendLine()
+            .Append("StringBuliderTwo")
+            .AppendLine()
+            .Append('*', 7)
+            .Remove(10, 3)
+            .Insert(5, new string('/', 7))
+            .Replace('/', '_',7,3);
+
+
+
+        Console.WriteLine(builderTwo);
+
+
+    }
+}
