66de7d8d6f7f79d0da11d4bcb54031ac809aef12
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-05 09:51:03 -0600

    modified:   Concept/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs modified:   Concept/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfoInputs.cache

--- Concept/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ Concept/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70526bc1af611d4deeb8322679b3f5c16dcad07f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22bce109ae3a4d4b9e3945484e295bbc6c5032ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
