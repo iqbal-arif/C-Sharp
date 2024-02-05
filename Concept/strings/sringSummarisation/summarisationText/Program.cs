e253d2d50a549e28bde50accf3d33426554218fc
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-05 12:54:34 -0600

    new file:   Concept/strings/sringSummarisation/.gitignore

---
+++ Concept/strings/sringSummarisation/summarisationText/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Text Summarisation!");
+
+var sentences = "This is going to be a very extra ordinary perfecdt example of a very very very long paragrah."
+const int maxLength = 20;
+
+if (sentences.Length < maxLength){
+    Console.WriteLine(sentences);
+}
