e47148772ac5d75c8036a77bc7b6bb104796c499
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-07 12:03:07 -0600

    Moved Class Method notes to Note folder

--- Concept/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ Concept/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a471e1d9b66e1de2747caabda3ef4a59e45c7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa62a4aca98f3fecdf850c9e3500228e71d038a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
